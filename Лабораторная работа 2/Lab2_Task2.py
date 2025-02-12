389cee8b87219f797ba2249a5923a75c7e347dc1
Penkov Viktor <144222952+ViktorP08@users.noreply.github.com>
2025-02-12 20:11:01 +0300

    Lab2_Task2.py

--- Лабораторная работа 2/Lab2_Task2.py
+++ Лабораторная работа 2/Lab2_Task2.py
@@ -1,10 +1,15 @@
+from math import ceil
+
 salary = 5000  # Ежемесячная зарплата
 spend = 6000  # Траты за первый месяц
 months = 10  # Количество месяцев, которое планируется протянуть без долгов
 increase = 0.03  # Ежемесячный рост цен
 
 # TODO Рассчитайте подушку безопасности, чтобы протянуть 10 месяцев без долгов
+money_cap = 0
 
-money_capital = int(sum(max(spend * (1 + increase) ** month - salary, 0) for month in range(months)))
-
+for i in range(months):
+    money_cap += spend - salary
+    spend *= 1 + increase
+money_capital = ceil(money_cap)
 print(f"Подушка безопасности, чтобы протянуть {months} месяцев без долгов:", money_capital)
